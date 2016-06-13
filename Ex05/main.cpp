0fc3906367e6116c4389f42157d9c2d1dcc26e94
allymcconnell <amcconnell16@my.whitworth.edu>
2016-06-13 07:21:05 -0700

    Initial Commit

---
+++ Ex05/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ex05
+//
+//  Created by Ally McConnell on 6/13/16.
+//  Copyright © 2016 Ally McConnell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
